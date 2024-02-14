c53083b39d7e5e7b7e815b08e32801f2dae4a124
devcarolinealmeida <carolineitalia85@gmail.com>
2024-02-14 14:43:49 +0100

    add config init and nav-menu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavMenu from './components/NavMenu/NavMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <NavMenu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavMenu/NavMenu.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './NavMenu.css';
+
+function NavMenu() {
+  return (
+    <Navbar expand="lg" className="navbar-dark bg-nav">
+      <Container>
+        <Navbar.Brand className='txt-primary' href="#home">&#123; caroline &#125;</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contacts">Contacts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavMenu;
