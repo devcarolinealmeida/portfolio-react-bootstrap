d92246ebadfac6c6a8bb12109dd2c38c644cd719
devcarolinealmeida <carolineitalia85@gmail.com>
2024-03-03 17:31:49 +0100

    update initial heigth

--- src/components/Initial/Initial.jsx
+++ src/components/Initial/Initial.jsx
@@ -6,12 +6,12 @@ import { PiCoffee } from "react-icons/pi"
 export default function Initial() {
   return (
     <div
-      className="container-fluid d-flex align-items-center min-vh-100"
+      className="container-fluid"
       style={{ background: "var(--darkGreen)" }}
     >
-      <div className="container d-flex flex-column justify-content-between" style={{minHeight: '90vh'}}>
+      <div className="container d-flex flex-column justify-content-between min-vh-100 py-4">
         <div className="row justify-content-between align-items-lg-center">
-          <div className="col col-lg-3 fw-bold" style={{fontSize: 'clamp(1rem, 0.9044rem + 0.3922vw, 1.375rem)'}}>
+          <div className="col col-lg-3 fw-bold" style={{fontSize: 'clamp(0.9375rem, 0.826rem + 0.4575vw, 1.375rem)'}}>
             &#123; <span className="txt-primary">Caroline Almeida</span> &#125;
           </div>
           <div className="col col-lg-7 d-flex flex-column flex-md-row justify-content-md-between align-items-end fs-sm">
