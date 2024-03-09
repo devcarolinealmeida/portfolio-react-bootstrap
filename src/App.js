d4925d104a15071dcf1954a8f0d944aac3d8757b
devcarolinealmeida <carolineitalia85@gmail.com>
2024-03-09 17:29:12 +0100

    update container ParallaxWrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import About from './components/About/About';
-import Footer from './components/Footer/Footer';
 import Initial from './components/Initial/Initial';
 import ParallaxWrap from './components/ParallaxWrap/ParallaxWrap';
-import NavMenu from './components/NavMenu/NavMenu';
-import Works from './components/Works/Works';
-import OtherProjects from './components/OthersProjects/OtherProjects';
 
 function App() {
   return (
     <div className="App">
       <Initial/>
-      <NavMenu/>
-      <Works/>
-      {/* <ParallaxWrap/> */}
-      <OtherProjects/>
+      {/* <NavMenu/>
+      <Works/> */}
+      <ParallaxWrap/>
+  {/*     <OtherProjects/>
       <About/>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
--- src/components/ParallaxWrap/ParallaxWrap.jsx
+++ src/components/ParallaxWrap/ParallaxWrap.jsx
@@ -1,60 +1,35 @@
-import React, { useRef } from "react";
-import Works from "../Works/Works";
-
-import { motion, useScroll, useTransform } from "framer-motion";
-import "./ParallaxWrap.css";
+import React from "react";
 import NavMenu from "../NavMenu/NavMenu";
-import Initial from "../Initial/Initial";
-import About from "../About/About";
+import Works from "../Works/Works";
+import OtherProjects from '../OthersProjects/OtherProjects';
+import About from '../About/About';
+import Footer from '../Footer/Footer';
+import "./ParallaxWrap.css";
 
 export default function ParallaxWrap() {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["0 1", "1.33 1"],
-    
-  });
-  const yBlur = useTransform(scrollYProgress, [0, 1], [0, -500]);
-  const y = useTransform(scrollYProgress, [0, 1], [50, -200]);
-
   return (
-    <div className="_container" 
-    style={{
-      border: '1px solid yellow',
-      position:'relative', 
-      minHeight:'120rem'
-      }}>
-      <motion.div /* className={"_parallaxWrapper"}  */
-      className="_motion-div-1"
-      ref={ref}
-      style={{ 
-        border: '1px solid red',
-        translateY:yBlur,
-        backgroundColor: 'transparent',
-        position: 'absolute',
-        width: '100%',
-        top: '0',
-        height: '100%'
-        }} >
-        <div className="_bg-Blur" 
+    <div className="_parallaxWrap">
+      <div
+        className="_bg-Blur"
         style={{
-          border: '1px solid purple',
-          width: '100%',
-          minHeight: '100vh',
-        }}></div>
-        <motion.div className="_container-nav-projects" 
+          width: "100%",
+          minHeight: "100vh",
+        }}
+      ></div>
+      <div
+        className="_containerPags"
         style={{
-          border: '1px solid green',
-          translateY:y, 
-          position:'absolute', 
-          background:'black',
-          width: '100%',
-          minHeight: '100%'}}>
-          <NavMenu/>
-          <Works/>
-        </motion.div>
-      </motion.div>
-      
+          background: "black",
+          width: "100%",
+          minHeight: "100%",
+        }}
+      >
+        <NavMenu />
+        <Works />
+        <OtherProjects />
+        <About />
+        <Footer />
+      </div>
     </div>
   );
 }
